224a3080a3f93eb7e13a3ffeee55a8d7d3c9f415
Hellol77 <dhe7700@naver.com>
2022-02-22 21:53:21 +0900

    회원 인증 기능

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const User = require("./models/User");
 const { userInfo } = require("os");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
+const { auth } = require("./middleware/auth");
 const http = require("http").createServer(app);
 require("dotenv").config();
 
@@ -76,6 +77,18 @@ app.post("/api/users/login", (req, res) => {
           .json({ loginSuccess: true, userId: user._id });
       });
     });
+  });
+});
+app.get("/api/users/auth", auth, function (req, res) {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
